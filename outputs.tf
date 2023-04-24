667f11edf3f7970c5615ad7292dbd89ab869b00c
Martin Linxfeld <44723865+mlinxfeld@users.noreply.github.com>
2023-04-24 17:48:51 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,11 @@ output "FoggyKitchenContainerInstancePublicIP" {
    value = data.oci_core_vnic.FoggyKitchenContainerInstanceVnic.public_ip_address
 }
 
+output "FoggyKitchenContainerInstance_VNIC1_OCID" {
+  value = oci_container_instances_container_instance.FoggyKitchenContainerInstance.vnics[0].vnic_id
+}
+
+output "FoggyKitchenContainerInstance_IPS1" {
+  value = data.oci_core_private_ips.FoggyKitchenContainerInstance_IPS1.private_ips[0]
+}
+
