9b53f8c7369340f979f149a45722da24a9ecdd0c
Martin Linxfeld <44723865+mlinxfeld@users.noreply.github.com>
2023-04-28 14:16:17 +0200

    Update datasources.tf

--- datasources.tf
+++ datasources.tf
@@ -32,11 +32,19 @@ data "oci_objectstorage_namespace" "test_namespace" {
 
 
 data "oci_core_vnic" "FoggyKitchenContainerInstanceVnic" {
+  depends_on = [
+    oci_container_instances_container_instance.FoggyKitchenContainerInstance, 
+    oci_core_public_ip.FoggyKitchenContainerInstance_PublicReservedIP
+  ]
   provider   = oci.targetregion
   vnic_id = oci_container_instances_container_instance.FoggyKitchenContainerInstance.vnics[0].vnic_id
 }
 
 data "oci_core_private_ips" "FoggyKitchenContainerInstance_IPS1" {
+  depends_on = [
+    oci_container_instances_container_instance.FoggyKitchenContainerInstance, 
+    oci_core_public_ip.FoggyKitchenContainerInstance_PublicReservedIP
+  ]
   provider   = oci.targetregion
   vnic_id    = oci_container_instances_container_instance.FoggyKitchenContainerInstance.vnics[0].vnic_id
   subnet_id  = oci_core_subnet.FoggyKitchenContainerInstanceSubnet.id
