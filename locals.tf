dcc0193a7246f5bada89000905cad8ce74444438
Martin Linxfeld <44723865+mlinxfeld@users.noreply.github.com>
2023-04-28 13:58:56 +0200

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -6,6 +6,8 @@ locals {
   icmp_protocol_number                    = "1"
   all_protocols                           = "all"
 
-  ocir_docker_repository   = join("", [lower(lookup(data.oci_identity_regions.oci_regions.regions[0], "key")), ".ocir.io"])
-  ocir_namespace           = lookup(data.oci_objectstorage_namespace.test_namespace, "namespace")
+  ocir_docker_repository        = join("", [lower(lookup(data.oci_identity_regions.oci_regions.regions[0], "key")), ".ocir.io"])
+  ocir_namespace                = lookup(data.oci_objectstorage_namespace.test_namespace, "namespace")
+  container_instance_public_url = (var.nginx_port == "80") ? "http://${data.oci_core_vnic.FoggyKitchenContainerInstanceVnic.public_ip_address}" : "http://${data.oci_core_vnic.FoggyKitchenContainerInstanceVnic.public_ip_address}:${var.nginx_port}/"
+  container_instance_url        = !var.enable_ephemeral_public_ip && !var.enable_reserved_public_ip ? "" : local.container_instance_public_url
 }
