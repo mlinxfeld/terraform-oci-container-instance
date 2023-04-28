ec703ae264de93358f1c7befe3765ea60bda5d29
Martin Linxfeld <44723865+mlinxfeld@users.noreply.github.com>
2023-04-28 14:34:12 +0200

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -8,5 +8,4 @@ locals {
 
   ocir_docker_repository        = join("", [lower(lookup(data.oci_identity_regions.oci_regions.regions[0], "key")), ".ocir.io"])
   ocir_namespace                = lookup(data.oci_objectstorage_namespace.test_namespace, "namespace")
-  container_instance_url        = !var.enable_ephemeral_public_ip && !var.enable_reserved_public_ip ? "" : "http://${coalesce(data.oci_core_vnic.FoggyKitchenContainerInstanceVnic.public_ip_address, "")}:${var.nginx_port}/"
 }
